9cc03d76a20f7bb8b6c0f7313a115457eea4075e
Grace Mihiral <mihiralgrace@gmail.com>
2026-09-12 23:10:30 -0400

    testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,8 @@ def network_enviroment():
         subprocess.run(["sudo", "ip", "link", "set", iface, "up"], check=True)
     
     print("launching switch")
-    switch_process = subprocess.Popen(["sudo", "./build/switch"], preexec_fn=os.setpgrp)
+    switch_process = subprocess.Popen(["./build/switch"], preexec_fn=os.setpgrp)
+    switch_pgid = os.getpgid(switch_process.pid)
 
     time.sleep(0.5)
 
@@ -25,10 +26,11 @@ def network_enviroment():
 
     print("Shutting down switch")
     try:
-        os.killpg(os.getpgid(switch_process.pid), signal.SIGTERM)
+        os.killpg(switch_pgid, signal.SIGTERM)
         switch_process.wait(timeout=2)
     except Exception:
-        os.killpg(os.getpgid(switch_process.pid), signal.SIGKILL)
+        os.killpg(switch_pgid, signal.SIGKILL)
+        switch_process.wait()
 
     for iface in interfaces:
         subprocess.run(["sudo", "ip", "tuntap", "del", "mode", "tap", iface], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
